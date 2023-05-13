5311ccbfc390873d5db12d937e79fa68db7eda44
Androidmini9x <androidmini92@gmail.com>
2023-05-13 15:48:56 +0300

    Fix: Bugs

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,114 +1,50 @@
 #!/usr/bin/python3
-"""test for the console"""
-import unittest
-from unittest.mock import patch
-from io import StringIO
-import os
-import json
-import console
-import tests
-from console import HBNBCommand
-from models.base_model import BaseModel
-from models.user import User
+"""Test Console"""
 from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
-from models.city import City
-from models.engine.file_storage import FileStorage
-from models import storage
+import unittest
 
 
-class TestHbnbConsole(unittest.TestCase):
-    """this will all the
-    aspects about console"""
+class TestConsole(unittest.TestCase):
+    """
+    This test checks if all required classes
+    are created correctly.
+    """
+    def test_class(self):
+        """
+        This test checks if all required classes
+        are present
+        """
+        city1 = City()
+        amenity1 = Amenity()
+        state1 = State()
+        rev1 = Review()
+        place1 = Place()
+        self.assertEqual(city1.__class__.__name__, "City")
+        self.assertEqual(amenity1.__class__.__name__, "Amenity")
+        self.assertEqual(state1.__class__.__name__, "State")
+        self.assertEqual(rev1.__class__.__name__, "Review")
+        self.assertEqual(place1.__class__.__name__, "Place")
 
-    def setUp(self):
-        """Sets up tests
-        for the console methods."""
-        pass
-
-
-class TestHBNB_prompt(unittest.TestCase):
-
-    def testprompt(self):
-        self.assertEqual("(hbnb) ", HBNBCommand.prompt)
-
-    def test_emptyline(self):
-        with patch("sys.stdout", new=StringIO()) as output:
-            self.assertFalse(HBNBCommand().onecmd(""))
-            self.assertEqual("", output.getvalue().strip())
-
-
-class TestHBNBcreate(unittest.TestCase):
-
-    @classmethod
-    def setUp(self):
-        try:
-            os.rename("file.json", "tmp")
-        except IOError:
-            pass
-        FileStorage.__objects = {}
-
-    @classmethod
-    def tearDown(self):
-        try:
-            os.remove("file.json")
-        except IOError:
-            pass
-        try:
-            os.rename("tmp", "file.json")
-        except IOError:
-            pass
-
-    def test_create_missing_class(self):
-        correct = "** class name missing **"
-        with patch("sys.stdout", new=StringIO()) as output:
-            self.assertFalse(HBNBCommand().onecmd("create"))
-            self.assertEqual(correct, output.getvalue().strip())
-
-    def test_create_invalid_class(self):
-        correct = "** class doesn't exist **"
-        with patch("sys.stdout", new=StringIO()) as output:
-            self.assertFalse(HBNBCommand().onecmd("create MyModel"))
-            self.assertEqual(correct, output.getvalue().strip())
-
-    def test_create_object(self):
-        with patch("sys.stdout", new=StringIO()) as output:
-            self.assertFalse(HBNBCommand().onecmd("create BaseModel"))
-            self.assertLess(0, len(output.getvalue().strip()))
-            testKey = "BaseModel.{}".format(output.getvalue().strip())
-            self.assertIn(testKey, storage.all().keys())
-        with patch("sys.stdout", new=StringIO()) as output:
-            self.assertFalse(HBNBCommand().onecmd("create User"))
-            self.assertLess(0, len(output.getvalue().strip()))
-            testKey = "User.{}".format(output.getvalue().strip())
-            self.assertIn(testKey, storage.all().keys())
-        with patch("sys.stdout", new=StringIO()) as output:
-            self.assertFalse(HBNBCommand().onecmd("create State"))
-            self.assertLess(0, len(output.getvalue().strip()))
-            testKey = "State.{}".format(output.getvalue().strip())
-            self.assertIn(testKey, storage.all().keys())
-        with patch("sys.stdout", new=StringIO()) as output:
-            self.assertFalse(HBNBCommand().onecmd("create City"))
-            self.assertLess(0, len(output.getvalue().strip()))
-            testKey = "City.{}".format(output.getvalue().strip())
-            self.assertIn(testKey, storage.all().keys())
-        with patch("sys.stdout", new=StringIO()) as output:
-            self.assertFalse(HBNBCommand().onecmd("create Amenity"))
-            self.assertLess(0, len(output.getvalue().strip()))
-            testKey = "Amenity.{}".format(output.getvalue().strip())
-            self.assertIn(testKey, storage.all().keys())
-        with patch("sys.stdout", new=StringIO()) as output:
-            self.assertFalse(HBNBCommand().onecmd("create Place"))
-            self.assertLess(0, len(output.getvalue().strip()))
-            testKey = "Place.{}".format(output.getvalue().strip())
-            self.assertIn(testKey, storage.all().keys())
-        with patch("sys.stdout", new=StringIO()) as output:
-            self.assertFalse(HBNBCommand().onecmd("create Review"))
-            self.assertLess(0, len(output.getvalue().strip()))
-            testKey = "Review.{}".format(output.getvalue().strip())
-            self.assertIn(testKey, storage.all().keys())
+    def test_father(self):
+        """
+        This test checks if all required classes
+        inherit correcly from BaseModel
+        """
+        city1 = City()
+        amenity1 = Amenity()
+        state1 = State()
+        rev1 = Review()
+        place1 = Place()
+        self.assertTrue(issubclass(city1.__class__, BaseModel))
+        self.assertTrue(issubclass(amenity1.__class__, BaseModel))
+        self.assertTrue(issubclass(state1.__class__, BaseModel))
+        self.assertTrue(issubclass(rev1.__class__, BaseModel))
+        self.assertTrue(issubclass(place1.__class__, BaseModel))
 
 
 if __name__ == "__main__":
